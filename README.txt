5c8a86b1730c49dfe83ac9d0acd17f1839a36638
nimaaskarian <nimaaskarian.work@outlook.com>
2025-03-19 12:32:16 +0330

    Add compare option to contrast

--- README.txt
+++ README.txt
@@ -1,17 +1,18 @@
 simple set of cli utilities to compute color contrast and relative luminance
-<color> can be any string in form of #FFF or #FFFFFF or r,g,b
+<c> (<c-n>) can be any string in form of #FFF or #FFFFFF or r,g,b
 
 contrast:  prints contrast ratio of two colors
 [Usage]
-  contrast [Flags] <color-1> <color-2>
+  contrast [Flags] <c-1> <c-2>
 
 [Flags]
-  --wcag-check, -w                  check if passes the wcag standard and prints the status
+  --wcag-check, -w                       check if passes the wcag standard and prints the status
+  --compare <c-3>, -c <c-3>              check which of <c-1> or <c-2> has a higher contrast ratio with <c-3>, prints it (ignores other options)
 
 
 isdark: prints 1 if the color is dark, 0 if its not
 [Usage]
-  isdark [Flags] <color>
+  isdark [Flags] <c>
 
 [Flags]
   --print-luminance, -l             print relative luminance of color instead
@@ -19,7 +20,7 @@ isdark: prints 1 if the color is dark, 0 if its not
 
 cbetween: prints colors between two colors
 [Usage]
-  cbetween [Flags] <color-1> <color-2>
+  cbetween [Flags] <c-1> <c-2>
 
 [Flags]
   --include-self, -i                include the colors provided in the beginning and the end of the output
--- contrast.c
+++ contrast.c
@@ -7,16 +7,18 @@
 
 int main(int argc, char *argv[])
 {
-  bool wcag_check = 0;
+  bool wcag_check = false, compare = false;
   int c;
+  Color c1, c2, c3;
   while (true) {
     static struct option long_options[] =
           {
-            {"wcag-check",     no_argument,       0, 'w'},
+            {"wcag-check",     no_argument,          0, 'w'},
+            {"compare",        required_argument,    0, 'c'},
             {0, 0, 0, 0}
           };
     
-    c = getopt_long (argc, argv, "w", long_options, 0);
+    c = getopt_long (argc, argv, "wc:", long_options, 0);
     if (c == -1) {
       break;
     }
@@ -24,10 +26,14 @@ int main(int argc, char *argv[])
       case 0:
         break;
       case 'w':
-      wcag_check = true;
+        wcag_check = true;
+        break;
+      case 'c':
+        compare = true;
+        get_color(&c3, optarg);
+        break;
     }
   }
-  Color c1, c2;
   int opt = 0;
   if (argc - optind < 2) {
     fprintf(stderr, "no colors provided\n");
@@ -36,19 +42,29 @@ int main(int argc, char *argv[])
   get_color(&c1, argv[optind++]);
   get_color(&c2, argv[optind++]);
 
-  double ratio = contrast_ratio(c1, c2);
-  if (wcag_check) {
-    if (ratio >= 7.) {
-      puts("full AAA pass");
-    } else if (ratio >= 4.5) {
-      puts("large: AAA; normal: AA; graphical: pass");
-    } else if (ratio >= 3.) {
-      puts("large: AA; graphical: pass");
+  if (compare) {
+    double ratio1 = contrast_ratio(c1, c3);
+    double ratio2 = contrast_ratio(c2, c3);
+    if (ratio1 >= ratio2) {
+      print_color(c1);
     } else {
-      puts("failed");
+      print_color(c2);
     }
   } else {
-    printf("%.3f\n", ratio);
+    double ratio = contrast_ratio(c1, c2);
+    if (wcag_check) {
+      if (ratio >= 7.) {
+        puts("full AAA pass");
+      } else if (ratio >= 4.5) {
+        puts("large: AAA; normal: AA; graphical: pass");
+      } else if (ratio >= 3.) {
+        puts("large: AA; graphical: pass");
+      } else {
+        puts("failed");
+      }
+    } else {
+      printf("%.3f\n", ratio);
+    }
   }
 
   return EXIT_SUCCESS;
