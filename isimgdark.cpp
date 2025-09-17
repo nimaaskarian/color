36356b0a6ce859b0cf7826ebc1335d0386be2dc3
nimaaskarian <nimaaskarian.work@outlook.com>
2025-09-17 13:48:27 +0330

    feat: Add multiple argument support for isimgdark

--- isimgdark.cpp
+++ isimgdark.cpp
@@ -9,22 +9,24 @@
 
 int main(int argc, char *argv[])
 {
-  cv::Mat img = cv::imread(argv[1]);
-  std::vector<cv::Mat> three_channels;
-  cv::split(img, three_channels);
-  int dark = 0;
-  for(int i=0; i<img.rows; i++) {
-    for(int j=0; j<img.cols; j++)
-    {
-        Color color = {three_channels[0].at<uint8_t>(i, j), three_channels[1].at<uint8_t>(i, j), three_channels[2].at<uint8_t>(i, j)};
-        double luminance = relative_luminance(color);
-        if (luminance <= .5) {
-          dark+=1;
-        } else {
-          dark-=1;
-        }
+  for (int i = 1; argv[i]; i++) {
+    cv::Mat img = cv::imread(argv[i]);
+    std::vector<cv::Mat> three_channels;
+    cv::split(img, three_channels);
+    int dark = 0;
+    for(int i=0; i<img.rows; i++) {
+      for(int j=0; j<img.cols; j++)
+      {
+          Color color = {three_channels[0].at<uint8_t>(i, j), three_channels[1].at<uint8_t>(i, j), three_channels[2].at<uint8_t>(i, j)};
+          double luminance = relative_luminance(color);
+          if (luminance <= .5) {
+            dark+=1;
+          } else {
+            dark-=1;
+          }
+      }
     }
+    std::cout << (dark > 0) << ' ' << argv[i] << std::endl;
   }
-  std::cout << (dark > 0) << ' ' << argv[1] << std::endl;
   return 0;
 }
