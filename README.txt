10b9ff3d797af6436908a9540b3f9e33c1787ab1
nimaaskarian <nimaaskarian.work@outlook.com>
2025-09-17 13:55:06 +0330

    feat: Add --null flag too isimgdark

--- README.txt
+++ README.txt
@@ -12,7 +12,7 @@ contrast:  prints contrast ratio of two colors
 
 isdark: prints 1 if the color is dark, 0 if its not
 [Usage]
-  isdark [Flags] <c>
+  isdark [Flags] <c-1> <c-2> ... <c-n>
 
 [Flags]
   --print-luminance, -l             print relative luminance of color instead
@@ -30,3 +30,10 @@ cbetween: prints colors between two colors
 as-rgb: prints color as rgb
 [Usage]
   as-rgb <c>
+
+isimgdark: prints 1 if the image is dark, 0 if its not
+[Usage]
+isimgdark [Flags] <img1> <img2> ... <imgn>
+
+[Flags]
+  --null, -l                        print null delimited lines, instead of newlines. corresponds to grep -z, cut -z, xargs -0
--- isimgdark.cpp
+++ isimgdark.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <iostream>
 #include <vector>
+#include <getopt.h>
 #include <opencv4/opencv2/core/version.hpp>
 #include <opencv4/opencv2/core/cvdef.h>
 #include <opencv4/opencv2/core.hpp>
@@ -9,8 +10,28 @@
 
 int main(int argc, char *argv[])
 {
-  for (int i = 1; argv[i]; i++) {
-    cv::Mat img = cv::imread(argv[i]);
+  int c;
+  bool null = 0;
+  while (true) {
+    static struct option long_options[] =
+          {
+            {"null",     no_argument,       0, 'l'},
+            {0, 0, 0, 0}
+          };
+    
+    c = getopt_long (argc, argv, "0", long_options, 0);
+    if (c == -1) {
+      break;
+    }
+    switch (c) {
+      case 0:
+        break;
+      case 'l':
+      null = true;
+    }
+  }
+  for (; optind < argc; optind++) {
+    cv::Mat img = cv::imread(argv[optind]);
     std::vector<cv::Mat> three_channels;
     cv::split(img, three_channels);
     int dark = 0;
@@ -26,7 +47,11 @@ int main(int argc, char *argv[])
           }
       }
     }
-    std::cout << (dark > 0) << ' ' << argv[i] << std::endl;
+    if (null) {
+      std::cout << (dark > 0) << ' ' << argv[optind] << '\0';
+    } else {
+      std::cout << (dark > 0) << ' ' << argv[optind] << std::endl;
+    }
   }
   return 0;
 }
