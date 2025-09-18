37cc6520b2dc9191193ef1933dd329f4b6cdb8f5
nimaaskarian <nimaaskarian.work@outlook.com>
2025-09-18 18:23:16 +0330

    refactor: Refactor isimgdark

--- isimgdark.cpp
+++ isimgdark.cpp
@@ -38,26 +38,24 @@ int main(int argc, char *argv[])
   } else {
     end = '\n';
   }
-   std::vector<char*> items(argv + optind, argv + argc);
-   std::for_each(std::execution::par, items.begin(), items.end(), [end, print_luminance](char* s) {
-          cv::Mat img = cv::imread(s);
-          if (img.empty()) {
-              std::cerr << "error: failed to load image\n";
-              return;
-          }
-          cv::Mat img_srgb;
-          img.convertTo(img_srgb, CV_32F, 1.0/255);
-          std::vector<cv::Mat> bgr;
-          cv::split(img_srgb, bgr);
-          cv::Mat luminance_matrix = relative_luminance_rgb(bgr[2], bgr[1], bgr[0]);
-          double luminance = cv::mean(luminance_matrix)[0];
-          bool isdark = luminance < 0.5;
-          if (print_luminance) {
-            std::cout << luminance << ' ' << s << end;
-          } else {
-            std::cout << isdark << ' ' << s << end;
-          }
-        }
-    );
+  std::for_each(std::execution::par, argv + optind, argv + argc, [end, print_luminance](char* s) {
+    cv::Mat img = cv::imread(s);
+    if (img.empty()) {
+        std::cerr << "error: failed to load image\n";
+        return;
+    }
+    cv::Mat img_srgb;
+    img.convertTo(img_srgb, CV_32F, 1.0/255);
+    std::vector<cv::Mat> bgr;
+    cv::split(img_srgb, bgr);
+    cv::Mat luminance_matrix = relative_luminance_rgb(bgr[2], bgr[1], bgr[0]);
+    double luminance = cv::mean(luminance_matrix)[0];
+    bool isdark = luminance < 0.5;
+    if (print_luminance) {
+      std::cout << luminance << ' ' << s << end;
+    } else {
+      std::cout << isdark << ' ' << s << end;
+    }
+  });
   return 0;
 }
